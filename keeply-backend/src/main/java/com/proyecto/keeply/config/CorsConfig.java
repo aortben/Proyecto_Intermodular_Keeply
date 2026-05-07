941e23289b290cf1472a9c2ef4ae9807a6c4d741
aortben <alvaroortegabenitez03@gmail.com>
2026-05-07 19:11:03 +0200

    Arreglo CORS

--- keeply-backend/src/main/java/com/proyecto/keeply/config/CorsConfig.java
+++ keeply-backend/src/main/java/com/proyecto/keeply/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://proyecto-intermodular-keeply.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
